45392a046513b3cb9e67ef80855fb4ad6fb5f604
Heesub <howlongwillitlast14@gmail.com>
2015-02-19 19:14:40 +0900

    sd
    sd

--- src/article/api.py
+++
@@ -1,9 +0,0 @@
-from tastypie.resources import ModelResource
-from tastypie.constants import ALL
-from .models import Article
-
-class ArticleResource(ModelResource):
-	class Meta:
-		queryset = Article.objects.all()
-		resource_name = "article"  ##when it is called on our urls...
-		filtering = { "title" : ALL }
